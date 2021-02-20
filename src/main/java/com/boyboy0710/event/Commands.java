34dce7e486bc31933b1ee42c6288c59032d434b0
boyboy0710 <boyboy0710@naver.com>
2021-02-20 21:41:19 +0900

    zjal

--- src/main/java/com/boyboy0710/event/Commands.java
+++ src/main/java/com/boyboy0710/event/Commands.java
@@ -3,19 +3,29 @@ package com.boyboy0710.event;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabExecutor;
+import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.List;
 
-public class Commands implements TabExecutor {
+public class Commands extends JavaPlugin implements TabExecutor {
+    public void onEnable() {
+        getConfig().options().copyDefaults(true);
+        saveConfig();
+    }
+
+    public boolean BlockBreakEvent = getConfig().getBoolean("BlockBreakEvent");
+
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
         if(cmd.getName().equalsIgnoreCase("event")){
             if(args[0].equalsIgnoreCase("BlockBreakEvent")) {
                 if(args[1].equalsIgnoreCase("true")) {
-
+                    BlockBreakEvent = true;
+                    saveConfig();
                 }
                 else if(args[1].equalsIgnoreCase("false")) {
-
+                    BlockBreakEvent = false;
+                    saveConfig();
                 }
             }
         }
@@ -26,4 +36,5 @@ public class Commands implements TabExecutor {
     public List<String> onTabComplete(CommandSender sender, Command command, String alias, String[] args) {
         return null;
     }
+
 }
--- src/main/java/com/boyboy0710/event/Main.java
+++ src/main/java/com/boyboy0710/event/Main.java
@@ -4,14 +4,21 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Main extends JavaPlugin {
 
-    @Override
     public void onEnable() {
-        // Plugin startup logic
+        System.out.println("----------------------------------------------------------------------");
+        System.out.println("            Event 를러그인을 성공적으로 불러왔습니다");
+        System.out.println("               플러그인 제작자ㅣ:booyboy0710");
+        System.out.println("----------------------------------------------------------------------");
 
+        getCommand("event").setExecutor(new Commands()); //Executor을 등록하는 매소드
+        getCommand("event").setTabCompleter(new Commands()); //TabCompleter을 등록하는 매소드
+
+        getConfig().options().copyDefaults(true);
+        saveConfig();
     }
-
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+        System.out.println("Event 플러그인이 비활성화 되었습니다");
     }
+    public boolean BlockBreakEvent = getConfig().getBoolean("BlockBreakEvent");
 }
